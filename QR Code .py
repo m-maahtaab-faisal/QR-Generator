c0cc432ead4f642684c4e781fcc5510357b58384
MUHAMMAD MAHTAB FAISAL <cursedloop325@gmail.com>
2026-01-01 20:42:25 +0500

    Location Updated

--- QR Code .py
+++ QR Code .py
@@ -1,7 +1,7 @@
 import qrcode
 url = input("Enter the URL you want to make QR Code: ")
 filename = input("Enter Your File Name: ")
-file = f"C:\\Users\\Public\\Desktop\\{filename}.png"
+file = f"{filename}.png"
 qr = qrcode.QRCode()
 qr.add_data(url)
 img = qr.make_image()
